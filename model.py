7be3278a96b3180c436ad1f51428e10c5582ba1f
R10666 <55814052+R10666@users.noreply.github.com>
2024-03-26 13:12:37 -0700

    processed LayerNormalization

--- model.py
+++ model.py
@@ -42,7 +42,39 @@ class PositionalEncoding(nn.Module):
         #sin for even, cos for odd
         pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
         pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
+
         # Add a batch dimension to the positional encoding
         pe = pe.unsqueeze(0) # (1, seq_len, d_model)
         
         self.register_buffer('pe', pe) # save the positional encoding as a buffer
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) #add positional encoding to itself, also no learn since fixed
+        return self.dropout(x)
+
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6) -> None: #eps to make math stable if s.d is very small
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #multiply ,nn.Parameter allows for learning
+        self.bias = nn.Parameter(torch.zeros(1)) #added
+
+        def forward(self, x):
+            mean = x.mean(dim = -1, keepdim = True)
+            std = x.std(dim = -1, keepdim = True)
+            return self.alpha * (x - mean) / (std + self.eps) + self.bias #formula for normalization 
+
+    class FeedFordwardBlock(nn.Module):
+
+        def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+            super().__init__()
+            
+
+
+
+
+
+
