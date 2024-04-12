5c0857cd940717a83c863bede2bc4c12d0db0046
R10666 <55814052+R10666@users.noreply.github.com>
2024-04-11 23:08:05 -0700

    config change

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+## This file is essentially the training loop setup ##
 from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 8,
-        "num_epochs": 20,
-        "lr": 10**-4, #learning rate
-        "seq_len": 350,
-        "d_model": 512,
-        "lang_src": "en",
+        "batch_size": 8, # number of batch/traing example processed at once, so 8 sentence. 
+        "num_epochs": 20, # each epochs is 1 complete pass through of the dataset, so runs dataset 20 times
+        "lr": 10**-4, #Learning rate, determining the size of step taken in optimization. ##Note This can be dynamic and gradually changed throughout training.
+        "seq_len": 350, # max sequence length for input. Rough and safe estimate = 350
+        "d_model": 512, # diamention of hidden state, 512 from paper
+        "lang_src": "en", 
         "lang_tgt": "fr",
         "model_folder": "weights",
         "model_filename": "tmodel_",
