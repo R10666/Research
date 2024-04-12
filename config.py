5082f918f4ea1a3a75718539b6a07eea5b4a1590
R10666 <55814052+R10666@users.noreply.github.com>
2024-04-11 23:17:28 -0700

    config Final!

--- config.py
+++ config.py
@@ -8,16 +8,16 @@ def get_config():
         "lr": 10**-4, #Learning rate, determining the size of step taken in optimization. ##Note This can be dynamic and gradually changed throughout training.
         "seq_len": 350, # max sequence length for input. Rough and safe estimate = 350
         "d_model": 512, # diamention of hidden state, 512 from paper
-        "lang_src": "en", 
-        "lang_tgt": "fr",
-        "model_folder": "weights",
-        "model_filename": "tmodel_",
-        "preload": None,
-        "tokenizer_file": "tokenizer_{0}.json",
-        "experiment_name": "runs/tmodel"
+        "lang_src": "en", # source langauge
+        "lang_tgt": "fr", # output langauge
+        "model_folder": "weights", # name of folder where each training/epochs is stored
+        "model_filename": "tmodel_", # the name of file for each epoch
+        "preload": None, # set a name of file to load and restart train if crash 
+        "tokenizer_file": "tokenizer_{0}.json", # file for tokens
+        "experiment_name": "runs/tmodel" # just a name for tensorboard logging
     }
 
-
+#generate path and saves the weight files
 def get_weights_file_path(config, epoch: str):
     model_folder = config["model_folder"]
     model_basename = config["model_basename"]
