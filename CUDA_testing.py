0af3e3cbde37a0b20313214cd1a86a80016bc4ad
R10666 <55814052+R10666@users.noreply.github.com>
2024-04-14 18:10:31 -0700

    use this to test CUDA

--- CUDA_testing.py
+++ CUDA_testing.py
@@ -1,4 +1,8 @@
+#Run this to check if CUDA is working correctly
 import torch
 import sys
 print(sys.version)
 print(torch.cuda.is_available())
+
+#check if ur GPU is CUDA compatible here: https://developer.nvidia.com/cuda-gpus
+#Download CUDA here: https://developer.nvidia.com/cuda-downloads
